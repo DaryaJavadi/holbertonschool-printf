39a882a010504c364988a86c1fff2229415d80ca
Darya Javadi <darya.javadi15@gmail.com>
2024-11-30 19:39:11 +0000

    printf

---
+++ _printf.c
@@ -0,0 +1,37 @@
+/**
+ * _printf - printf
+ * @format: the string format
+ * Return: length
+ */
+#include "main.h"
+#include <stdarg.h>
+int _printf(const char *format, ...)
+{
+	int i = 0;
+	unsigned int len = 0;
+	va_list args;
+
+	va_start(args, format);
+	while (format[i])
+	{
+		if (format[i] == '%' && format[i + 1] == 'c')
+			len += _putchar(va_arg(args, int)), i += 2;
+
+		else if (format[i] == '%' && format[i + 1] == 's')
+			len += print_string(va_arg(args, char *)), i += 2;
+
+		else if (format[i] == '%' && format[i + 1] == '%')
+			len += _putchar('%'), i += 2;
+
+		else if (format[i] == '%' && format[i + 1] == '\0')
+			len--, i++;
+
+		else if (format[i] == '%' && (format[i + 1] == 'd' || format[i + 1] == 'i'))
+			len += print_number(va_arg(args, int)), i += 2;
+
+		else
+			len += _putchar(format[i]), i++;
+	}
+	va_end(args);
+	return (len);
+}
---
+++ _putchar.c
@@ -0,0 +1,12 @@
+/**
+ * _putchar - writes the character to stdout
+ * @c: the character to print
+ *
+ * Return: on success 1.
+ * on error, -1 will returne
+ */
+#include <unistd.h>
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
